d4a49fd69d4462298fac0dfc7f83df54360352f8
Bohuslav Sedláček <sedlacekboza@gmail.com>
2026-04-01 22:32:52 +0200

    Add Microsoft Clarity tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -224,6 +224,13 @@ export default function RootLayout({
             gtag('config', 'AW-18028160012');
           `}
         </Script>
+        <Script id="clarity" strategy="afterInteractive">
+          {`(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "w3uehhbkde");`}
+        </Script>
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
